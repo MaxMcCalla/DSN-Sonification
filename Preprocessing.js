2be5253993a06b005a5ce33d82d423fd134a3069
MaxMcCalla <36572894+MaxMcCalla@users.noreply.github.com>
2026-02-02 19:23:35 -0500

    Update Preprocessing.js

--- Preprocessing.js
+++ Preprocessing.js
@@ -3,11 +3,14 @@ fetch('https://eyes.nasa.gov/dsn/data/dsn.json')
   .then(data => {
     var i = 0;
     for(const dish in data.dishes){
-    i++;
-    console.log(data.dishes[dish].desc)
-    console.log("antenna"+i)
-    const element = document.getElementById("antenna"+i);
-    element.innerHTML = dish + data.dishes[dish].desc
+      i++;
+      console.log(data.dishes[dish].desc)
+      console.log("antenna"+i)
+      const element = document.getElementById("antenna"+i);
+      element.innerHTML = dish + " " + data.dishes[dish].desc
+      for(const sig in dish.sigs){
+        console.log(sig.active)
+      }
     }
   }) // Work with the parsed data
   .catch(error => console.error('Error fetching data:', error)); // Handle network errors
