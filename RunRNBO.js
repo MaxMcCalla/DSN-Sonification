59dc3826b0593dc4eacc4af5024bcf3cb5d7d8c5
MaxMcCalla <36572894+MaxMcCalla@users.noreply.github.com>
2026-02-09 20:25:21 -0500

    Update RunRNBO.js

--- RunRNBO.js
+++ RunRNBO.js
@@ -1,1 +1,21 @@
+// Get createDevice from the rnbo.js library
+const { createDevice } = require("@rnbo/js");
 
+// Create AudioContext
+let WAContext = window.AudioContext || window.webkitAudioContext;
+let context = new WAContext();
+
+const setup = async () => {
+    let rawPatcher = await fetch("patcher.export.json");
+    let patcher = await rawPatcher.json();
+
+    let device = await createDevice({ context, patcher });
+
+    // This connects the device to audio output, but you may still need to call context.resume()
+    // from a user-initiated function.
+    device.node.connect(context.destination);
+};
+
+// We can't await an asynchronous function at the top level, so we create an asynchronous
+// function setup, and then call it without waiting for the result.
+setup();
